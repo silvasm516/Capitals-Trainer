da018a61a65a12049a4e6061bd9304eec5253efc
Steve Silva <silvasm516@aol.com>
2025-10-24 03:06:02 -0700

    server serves template

--- Capitals_Trainer/settings.py
+++ Capitals_Trainer/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'Capitals_Trainer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': (BASE_DIR / 'db.sqlite3'),
     }
 }
 
