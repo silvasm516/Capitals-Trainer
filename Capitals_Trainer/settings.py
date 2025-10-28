254447ac4e3613420045780b1aa78575bf89f701
Steve Silva <silvasm516@aol.com>
2025-10-27 19:55:26 -0700

    completed but without footer

--- Capitals_Trainer/settings.py
+++ Capitals_Trainer/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Capitals_Trainer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'Capitals_Trainer', 'templates', 'Capitals(2).html'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- trainer/urls.py
+++ trainer/urls.py
@@ -4,6 +4,6 @@ from .views import HomePageView
 
 urlpatterns = [
 
-    path('Hom', HomePageView.as_view(), name='Capitals(2)'),
+    path('Hom', HomePageView.as_view(), name='Hom'),
 
     ]
