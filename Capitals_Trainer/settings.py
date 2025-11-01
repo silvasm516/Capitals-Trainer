d579a84388be4921f3621b2c8492e9c35595e7cf
Steve Silva <silvasm516@aol.com>
2025-10-31 21:53:46 -0700

    modified Settings

--- Capitals_Trainer/settings.py
+++ Capitals_Trainer/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Capitals_Trainer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'Capitals_Trainer', 'templates'],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
